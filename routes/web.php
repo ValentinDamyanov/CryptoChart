e411d6cd1c950355ced14da184f103c61c29c1b1
Valentin Damyanov <valentin@sapirbg.com>
2022-04-19 09:11:13 +0300

    Beautifly urls in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/fake/{num}', 'HomeController@faker');
-Route::get('/', 'HomeController@index');
-Route::post('/add_to_price_alert', 'HomeController@addToPriceAlert');
+Route::get('/fake/{num}', [HomeController::class, 'faker']);
+Route::get('/', [HomeController::class, 'index']);
+Route::post('/add_to_price_alert', [HomeController::class, 'addToPriceAlert']);
